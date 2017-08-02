94302fe9ec74388d29b7a22249fba8317130326f
Tiafeno <tiafenofnel@gmail.com>
2017-08-02 14:44:10 +0300

    [ FIX ] Fix backgound on min-width 1600px

--- header.php
+++ header.php
@@ -18,8 +18,37 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<?php endif; ?>
+  <?php endif; ?>
+  <script type="text/javascript">
+    var PAGE = "Home";
+  </script>
   <?php wp_head(); ?>
+
+  <style type="text/css">
+    ul.category-nav-offcanvas {
+      padding-right: 29px;
+    }
+    ul.category-nav-offcanvas > li {
+      float: left;
+      list-style: none;
+      padding-left: 14px;
+    }
+    ul.category-nav-offcanvas > li > a{
+      height: inherit;
+      justify-content: space-between;
+      color: #00A6DE;
+      font-weight: 500;
+      font-size: 0.685rem !important;
+    }
+    .header-offcanvas-title {
+      padding-left: 43px;
+    }
+    .header-category-nav-offcanvas .section-offcanvas{
+      -webkit-transition: width 2s, height 2s, background-color 2s, -webkit-transform 2s;
+      transition: transform 2s;
+    }
+  </style>
+
 </head>
 
 <body <?php body_class(); ?>>
@@ -27,7 +56,7 @@
     <header class="container-nav-top">
       <div class="uk-container uk-container-small animated  slideInDown">
         <div class="uk-text-left" uk-grid>
-            <div class="uk-width-1-4">
+            <div class="uk-width-1-5">
                 <div class="uk-card uk-card-default uk-card-body navbar-card-slogan">
                   <!--<p>DAVID ALEXANDRE CALMEL</p> -->
                   <p class="blogingo"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">DAVID ALEXANDRE CALMEL <?php //bloginfo( 'name' ); ?></a></p>
@@ -36,28 +65,31 @@
         </div>
       </div>
     </header>
-    <header class="header-nav-down sticky-menu">
+    <header class="header-nav-down">
       <div class="uk-container uk-container-small animated  slideInLeft HeaderSlogan">
-        <div class="uk-text-left onStickyStartHide" uk-grid>
-            <div class="uk-width-3-4 ">
+        <div class="uk-text-left onStickyStartHide">
+            <div class="uk-child-width-1-1@m uk-width-2-3@m uk-light" uk-grid>
+              <div>
                 <div class="uk-card uk-card-default uk-card-body navbar-card-slogan uk-animation-slide-top-small">
                   <?php
                   $description = get_bloginfo( 'description', 'display' );
         					if ( $description || is_customize_preview() ) : ?>
-                  <span>EXPERIENCED AGENCY MANAGER IN A MULTICULTURAL CONTEXT
+                  <span >EXPERIENCED AGENCY MANAGER IN A MULTICULTURAL CONTEXT
                   EXECUTIVE CREATIVE DIRECTOR, ART DIRECTOR & COPY WRITER
                   CREDENTIALS ON GLOBAL BRANDS, OPEN MINDED, PASSIONATED</span>
                   <?php //echo $description; ?>
                   <?php endif; ?>
 
                 </div>
+              </div>
             </div>
         </div>
 
       </div>
-      <div class="uk-container uk-container-small animated  slideInRight">
-        <nav class="uk-navbar-transparent uk-navbar" uk-navbar>
-
+    </header>
+    <header class="sticky-menu HeaderMenu">
+      <div class="uk-container uk-container-large">
+        <nav class="uk-navbar-transparent uk-navbar uk-container uk-container-small animated  slideInRight" uk-navbar>
           <div class="uk-navbar-left">
             <ul class="uk-navbar-nav">
                 <li class="uk-active">
@@ -70,15 +102,15 @@
 
           <div class="uk-navbar-right">
             <?php if ( has_nav_menu( 'primary' ) ) : ?>
-								<?php
-									wp_nav_menu( array(
+                <?php
+                  wp_nav_menu( array(
                     'menu_class' => 'uk-navbar-nav uk-visible@m',
                     'container_class' => '',
-										'theme_location' => 'primary',
+                    'theme_location' => 'primary',
                     'container_class' => 'container_class_menu'
-									 ) );
-								?>
-						<?php endif; ?> <!-- .main-navigation -->
+                    ) );
+                ?>
+            <?php endif; ?> <!-- .main-navigation -->
 
             <div class="uk-offcanvas-content uk-hidden@m">
                 <!-- The whole page content goes here -->
@@ -92,15 +124,15 @@
                         <div class="uk-card uk-card-secondary">
 
                           <?php if ( has_nav_menu( 'primary' ) ) : ?>
-              								<?php
-              									wp_nav_menu( array(
+                              <?php
+                                wp_nav_menu( array(
                                   'menu_class' => 'uk-nav uk-nav-default',
                                   'container_class' => '',
-              										'theme_location' => 'primary',
+                                  'theme_location' => 'primary',
                                   'container_class' => 'uk-card-body'
-              									 ) );
-              								?>
-              						<?php endif; ?> <!-- .offcanvas main-navigation -->
+                                  ) );
+                              ?>
+                          <?php endif; ?> <!-- .offcanvas main-navigation -->
                         </div>
                     </div>
                 </div>
@@ -111,6 +143,7 @@
         </nav>
       </div>
     </header>
+    
 
   </header>
 
