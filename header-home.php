d2d7cf7de67ebc519c5a066503e5022fb01a7c05
Tiafeno <tiafenofnel@gmail.com>
2017-08-14 16:05:27 +0300

    Fix animation frame header

--- header-home.php
+++ header-home.php
@@ -19,9 +19,6 @@
   <?php if ( is_singular() && pings_open( get_queried_object() ) ) : ?>
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
   <?php endif; ?>
-  <script type="text/javascript">
-    var PAGE = "Home";
-  </script>
   <?php wp_head(); ?>
 
   <style type="text/css">
@@ -65,96 +62,92 @@
         </div>
       </div>
     </header>
-    <header class="header-nav-down animated slideInDown">
-      <div class="uk-container uk-container-small HeaderSlogan">
-        <div class="uk-text-left onStickyStartHide">
-            <div class="uk-child-width-1-1@m uk-width-2-3@m uk-light" uk-grid>
-              <div>
-                <div class="uk-card uk-card-default uk-card-body navbar-card-slogan uk-animation-slide-top-small">
-                  <?php
-                  $description = get_bloginfo( 'description', 'display' );
-        					if ( $description || is_customize_preview() ) : ?>
-                  <span >EXPERIENCED AGENCY MANAGER IN A MULTICULTURAL CONTEXT
-                  EXECUTIVE CREATIVE DIRECTOR, ART DIRECTOR & COPY WRITER
-                  CREDENTIALS ON GLOBAL BRANDS, OPEN MINDED, PASSIONATED</span>
-                  <?php //echo $description; ?>
-                  <?php endif; ?>
+    <header style="background-color: #2D2E83">
+      <header class="header-nav-down animated slideInDown">
+        <div class="uk-container uk-container-small HeaderSlogan">
+          <div class="uk-text-left onStickyStartHide">
+              <div class="uk-child-width-1-1@m uk-width-2-3@m uk-light" uk-grid>
+                <div>
+                  <div class="uk-card uk-card-default uk-card-body navbar-card-slogan uk-animation-slide-top-small">
+                    <?php
+                    $description = get_bloginfo( 'description', 'display' );
+                    if ( $description || is_customize_preview() ) : ?>
+                    <span >EXPERIENCED AGENCY MANAGER IN A MULTICULTURAL CONTEXT
+                    EXECUTIVE CREATIVE DIRECTOR, ART DIRECTOR & COPY WRITER
+                    CREDENTIALS ON GLOBAL BRANDS, OPEN MINDED, PASSIONATED</span>
+                    <?php //echo $description; ?>
+                    <?php endif; ?>
 
+                  </div>
                 </div>
               </div>
-            </div>
+          </div>
+
         </div>
-
-      </div>
-    </header>
-    <header class="sticky-menu HeaderMenu animated  slideInDown">
-      <div class="uk-container uk-container-large">
-        <nav class="uk-navbar-transparent uk-navbar uk-container uk-container-small " uk-navbar>
-          <div class="uk-navbar-left">
-            <ul class="uk-navbar-nav">
-                <!-- On the desktop  uk-visible@m -->
-                <li class="uk-active">
-                  <a href="#">
-                    <div class="uk-cover-container img-navbar-title" style="position: absolute; top: 0; left: 0;">
-                        <canvas width="328" height="45"></canvas>
-                       <img src="<?= get_template_directory_uri().'/assets/img/portfolio.png' ?>" uk-cover/>
-                    </div>
-                  
-                    <!-- <h1 class="navbar-title">PORTFOLIO </h1> -->
-                  </a>
-                </li>
-                <!-- On mobile -->
-                <!-- <li class="uk-active uk-inline uk-hidden@m">
-                  <a href="#" class="uk-inline">
-                    <h1 class="navbar-title" style="margin: 0; padding: 0">PORTFOLIO </h1>
-                  </a>
-                </li> -->
-            </ul>
-          </div>
-
-          <div class="uk-navbar-right">
-            <?php if ( has_nav_menu( 'primary' ) ) : ?>
-                <?php
-                  wp_nav_menu( array(
-                    'menu_class' => 'uk-navbar-nav uk-visible@m',
-                    'container_class' => '',
-                    'theme_location' => 'primary',
-                    'container_class' => 'container_class_menu'
-                    ) );
-                ?>
-            <?php endif; ?> <!-- .main-navigation -->
-
-            <div class="uk-offcanvas-content uk-hidden@m">
-                <!-- The whole page content goes here -->
-                <div class="uk-navbar-right">
-                    <a class="uk-navbar-toggle" uk-navbar-toggle-icon uk-toggle="target: #offcanvas-push"></a>
-                </div>
-                <div id="offcanvas-push" uk-offcanvas>
-                    <div class="uk-offcanvas-bar">
-                        <button class="uk-offcanvas-close" type="button" uk-close></button>
-                        <h3><?php _e( 'Menu', 'twentysixteen' ); ?></h3>
-                        <div class="uk-card uk-card-secondary">
-
-                          <?php if ( has_nav_menu( 'primary' ) ) : ?>
-                              <?php
-                                wp_nav_menu( array(
-                                  'menu_class' => 'uk-nav uk-nav-default',
-                                  'container_class' => '',
-                                  'theme_location' => 'primary',
-                                  'container_class' => 'uk-card-body'
-                                  ) );
-                              ?>
-                          <?php endif; ?> <!-- .offcanvas main-navigation -->
-                        </div>
-                    </div>
-                </div>
+      </header>
+      <header class="sticky-menu HeaderMenu animated  slideInDown">
+        <div class="uk-container uk-container-large">
+          <nav class="uk-navbar-transparent uk-navbar uk-container uk-container-small " uk-navbar>
+            <div class="uk-navbar-left">
+              <ul class="uk-navbar-nav">
+                  <!-- On the desktop  uk-visible@m -->
+                  <li class="uk-active">
+                    <a href="#">
+                      <div class="uk-cover-container img-navbar-title" style="position: absolute; top: 0; left: 0;">
+                          <canvas width="328" height="45"></canvas>
+                        <img src="<?= get_template_directory_uri().'/assets/img/portfolio.png' ?>" uk-cover/>
+                      </div>
+                      <h1 class="navbar-title uk-hidden">PORTFOLIO </h1>
+                    </a>
+                  </li>
+              </ul>
             </div>
 
-          </div>
+            <div class="uk-navbar-right">
+              <?php if ( has_nav_menu( 'primary' ) ) : ?>
+                  <?php
+                    wp_nav_menu( array(
+                      'menu_class' => 'uk-navbar-nav uk-visible@m',
+                      'container_class' => '',
+                      'theme_location' => 'primary',
+                      'container_class' => 'container_class_menu'
+                      ) );
+                  ?>
+              <?php endif; ?> <!-- .main-navigation -->
 
-        </nav>
-      </div>
+              <div class="uk-offcanvas-content uk-hidden@m">
+                  <!-- The whole page content goes here -->
+                  <div class="uk-navbar-right">
+                      <a class="uk-navbar-toggle" uk-navbar-toggle-icon uk-toggle="target: #offcanvas-push"></a>
+                  </div>
+                  <div id="offcanvas-push" uk-offcanvas>
+                      <div class="uk-offcanvas-bar">
+                          <button class="uk-offcanvas-close" type="button" uk-close></button>
+                          <h3><?php _e( 'Menu', 'twentysixteen' ); ?></h3>
+                          <div class="uk-card uk-card-secondary">
+
+                            <?php if ( has_nav_menu( 'primary' ) ) : ?>
+                                <?php
+                                  wp_nav_menu( array(
+                                    'menu_class' => 'uk-nav uk-nav-default',
+                                    'container_class' => '',
+                                    'theme_location' => 'primary',
+                                    'container_class' => 'uk-card-body'
+                                    ) );
+                                ?>
+                            <?php endif; ?> <!-- .offcanvas main-navigation -->
+                          </div>
+                      </div>
+                  </div>
+              </div>
+
+            </div>
+
+          </nav>
+        </div>
+      </header>
     </header>
+    
     
 
   </header>
--- template-home.php
+++ template-home.php
@@ -10,17 +10,14 @@
  get_header('home'); ?>
 
  <div id="primary"  class="uk-padding-remove-left uk-padding-remove-right">
-   <?php if ( have_posts() ) : 
-          get_template_part( 'sidebar','top');
-   ?>
+   <?php if ( have_posts() ) : get_template_part( 'sidebar','top'); ?>
      <div id="primary-content" >
      <?php
      while ( have_posts() ) : the_post();
-     
        get_template_part( 'tpls/content','favorite');
-
      endwhile;
-   ?> </div> <?php
+   ?> </div> 
+   <?php
    else :
      get_template_part( 'tpls/content', 'none' );
    endif;
