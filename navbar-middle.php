5f52a7ec1ffdf7be03139083bf76ef9c43128cce
Tiafeno <tiafenofnel@gmail.com>
2017-08-29 15:53:14 +0300

    add button lang translate
    ## Dependancy
    * Plugins "Polylang"

--- navbar-middle.php
+++ navbar-middle.php
@@ -1,3 +1,15 @@
+<?php
+global $post;
+$toEN = null;
+$toFR = null;
+if (function_exists( 'pll_get_post' )){
+  $toEN = get_permalink(pll_get_post($post->ID, 'fr'));
+  $toFR = get_permalink(pll_get_post($post->ID, 'en'));
+} else {
+  throw new WP_Error( 'broke', "Plugins polylang is not define" );
+}
+?>
+
 <div uk-grid>
   <div class="uk-width-2-3@s uk-light">
     <div>
@@ -13,12 +25,31 @@
   </div>
   <div class="uk-width-1-3@s" style="margin-top: 7px;">
     <div class="flag">
-      <a href="#gb" title="United Kingdom">
+      <a href="#en" class="dc-translate" data-lang="en" title="United Kingdom">
         <span class="uk-icon uk-icon-image" style="background-image: url(<?= get_template_directory_uri().'/images/GB.png' ?>);"></span>
       </a>
-      <a href="#fr" title="French">
+      <a href="#fr" class="dc-translate" data-lang="fr" title="French">
         <span class="uk-icon uk-icon-image" style="background-image: url(<?= get_template_directory_uri().'/images/FR.png' ?>);"></span>
       </a>
     </div>
+    <script type="text/javascript">
+      // <![CDATA[
+        (function( $ ){
+          $( document ).ready(function(){
+            var urls = {"en":"<?= $toFR ?>","fr":"<?= $toEN ?>"};
+            $( 'a.dc-translate' )
+              .click(function(){
+                var currentAddr = window.location.origin +  window.location.pathname;
+                var translate = $( this ).data( 'lang' );
+
+                var url = urls[ translate ];
+                if (currentAddr === url) return;
+                window.location.href = url;
+              });
+          });
+        })( jQuery )
+					
+			// ]]>
+    </script>
   </div>
 </div>
