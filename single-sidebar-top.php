0a9d7eee4ea5f71ab8f934e9c21da0cd4814a69d
Tiafeno <tiafenofnel@gmail.com>
2017-12-19 21:15:20 +0300

    Fix rubric link for multilange site

--- single-sidebar-top.php
+++ single-sidebar-top.php
@@ -1,17 +1,24 @@
 <?php
 
-global $post, $MODEL;
+global $post, $MODEL, $post_id;
+$indication = "slug";
+$current_lang = pll_current_language( $indication );
 
 function getTitleLang( $id ) {
-  $indication = "slug";
-  $current_lang = pll_current_language( $indication );
+  // Returns the post (or page) translation
   $trad_id = pll_get_post($id, $current_lang);
+  // Returns the post 
   $trad_post = get_post( (int)$trad_id );
   return $trad_post;
 }
 
-$post_id = $MODEL->getSettings('page_id', [ 'post_type', $post->post_type ]);
-$title = getTitleLang( $post_id );
+function getRubricLink( $id ) {
+  $_id_post = pll_get_post((int)$id, $current_lang);
+  return get_permalink($_id_post);
+}
+
+$id = $MODEL->getSettings('page_id', [ 'post_type', $post->post_type ]);
+$title = getTitleLang( $id );
 ?>
 
 <header class="header-category-nav-offcanvas uk-hidden@m">
@@ -23,14 +30,14 @@ $title = getTitleLang( $post_id );
             'container_class' => '',
             'theme_location' => 'secondary',
             'container_class' => 'container_class_menu sidebar-top',
-            'post_id' => $post_id,
+            'post_id' => $id,
             'walker' => new Secondary_Walker()
             ) );
         ?><!-- .secondary-navigation -->
     </div>
     <div class="uk-container" style="margin-bottom: 10px;">
       <h2 class="header-offcanvas-title uk-padding-remove uk-text-uppercase">
-        <a href="<?= get_permalink( $post_id ) ?>" style="color: white"><?= cleanString($title->post_title) ?></a>
+        <a href="<?= getRubricLink( $id ) ?>" style="color: white"><?= cleanString($title->post_title) ?></a>
       </h2>
     </div>
   </div>
@@ -42,7 +49,7 @@ $title = getTitleLang( $post_id );
       <div class="uk-navbar-left">
         <ul class="uk-navbar-nav category-title">
             <li class="uk-active">
-              <h2 class="uk-text-uppercase"><a href="<?= get_permalink( $post_id ) ?>" style="color: white"><?= cleanString($title->post_title)?></a></h2>
+              <h2 class="uk-text-uppercase"><a href="<?= getRubricLink( $id ) ?>" style="color: white"><?= cleanString($title->post_title)?></a></h2>
             </li>
         </ul>
       </div>
@@ -55,7 +62,7 @@ $title = getTitleLang( $post_id );
               'container_class' => '',
               'theme_location' => 'secondary',
               'container_class' => 'container_class_menu',
-              'post_id' => $post_id,
+              'post_id' => $id,
               'walker' => new Secondary_Walker()
               ) );
           ?><!-- .secondary-navigation -->
